34bf11faa8835a82313300ec23792733c0f0c6c9
serhii <serhiy.loboda@gmail.com>
2024-06-12 17:19:57 +0300

    created views for Comment

--- comment_application/views.py
+++ comment_application/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from comment_application.models import Comment
+from comment_application.serializers import CommentSerializer
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 25
+    max_page_size = 100
+
+
+class CommentListCreate(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["username", "email", "created_at"]
+    ordering = ["-created_at"]
+    pagination_class = OrderPagination
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Comment.objects.filter(parent__isnull=True)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    permission_classes = (IsAuthenticated,)
