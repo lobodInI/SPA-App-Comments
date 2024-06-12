76744d00236d701d30f75de236656887dc70843c
serhii <serhiy.loboda@gmail.com>
2024-06-12 17:19:51 +0300

    created serializer for Comment

---
+++ comment_application/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from comment_application.models import Comment
+
+
+# class CommentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Comment
+#         fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    replies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'username', 'email', 'home_page', 'text', 'parent', 'created_at', 'replies']
+
+    def get_replies(self, obj):
+        if obj.replies.exists():
+            return CommentSerializer(obj.replies.all(), many=True).data
+        return None
