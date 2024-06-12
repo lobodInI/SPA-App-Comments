fb6461e176ef3366f6d2b5514c17ee296f819801
serhii <serhiy.loboda@gmail.com>
2024-06-12 18:39:25 +0300

    added cache

--- spa_project/settings.py
+++ spa_project/settings.py
@@ -60,6 +60,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'spa_project.urls'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
