46af69c2502630892755286c4317b313161fd397
serhii <serhiy.loboda@gmail.com>
2024-06-12 17:18:13 +0300

    create model Comment

--- comment_application/models.py
+++ comment_application/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    home_page = models.URLField(blank=True, null=True)
+    text = models.TextField()
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="replies"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"{self.username} - {self.text[:20]}"
